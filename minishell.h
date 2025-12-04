dd1a218831949709dbe9f4d84fbc767d9afdc698
Matthew <116694677+AndrewTowers@users.noreply.github.com>
2025-12-04 17:28:34 +0000

    Token and input parsing improvements

--- minishell.h
+++ minishell.h
@@ -16,32 +16,6 @@
 # include <termios.h>
 # include "libft/libft.h"
 
-typedef enum e_token_type
-{
-	WORD,
-	PIPE,
-	REDIR_IN,      // '<'
-	REDIR_OUT,     // '>'
-	REDIR_APPEND,  // '>>'
-	HEREDOC,       // '<<'
-	END_OF_INPUT
-}	t_token_type;
-
-typedef struct s_env
-{
-	char			*key;
-	char			*value;
-	struct s_env	*next;
-}	t_env;
-
-typedef struct s_token//list
-{
-	t_token_type	type;
-	char			*value;
-	struct s_token	*next;
-	struct s_token	*prev;
-}	t_token;
-
 typedef struct s_cmd
 {
 	char			**args;
@@ -53,6 +27,43 @@ typedef struct s_cmd
 	t_token_type	redir_type;
 	struct s_cmd	*next;
 } t_cmd;
+
+typedef struct s_parse_word
+{
+	char	*buf;
+	char	quote;
+	int		start;
+	char	*segment;
+	char	*tmp;
+	char	*exp;
+}	t_parse_word;
+
+typedef enum e_token_type
+{
+	WORD,
+	PIPE,
+	REDIR_IN,      // '<'
+	REDIR_OUT,     // '>'
+	REDIR_APPEND,  // '>>'
+	HEREDOC,       // '<<'
+	END_OF_INPUT
+}	t_token_type;
+
+
+typedef struct s_token//list
+{
+	t_token_type	type;
+	char			*value;
+	struct s_token	*next;
+	struct s_token	*prev;
+}	t_token;
+
+typedef struct s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}	t_env;
 
 typedef struct s_mini
 {
@@ -68,7 +79,7 @@ typedef struct s_mini
 // global variable to intercept the signal
 extern int	g_signal_status;
 
-void			tokenize_input(char *input, t_env *env);
+t_token			*tokenize_input(char *input, t_env *env);
 
 void			add_token(t_token **list, t_token *new);
 t_token			*new_token(t_token_type type, char *value);
@@ -88,4 +99,5 @@ void	handle_sigint(int sig);
 
 // Funciones limpieza
 void	free_env_list(t_env *env_list);
+
 #endif
--- token.c
+++ token.c
@@ -1,20 +1,90 @@
 #include "minishell.h"
 
-char	parse_word(char *input, int *i, t_token *list)
+char	*expand_variable(char *input, int *i, t_env *env)
 {
-	int	start;
+	int		start;
+	char	*key;
+	t_env	*tmp;
 
-	start = *i;
-	while (input[*i] && input[*i] != ' ' && input[*i] != '\t' && !ispecial(&input[*i]))
+	start = ++(*i);
+	while (input[*i] && (ft_isalnum(input[*i] || input[*i] == '_')))
 		(*i)++;
-	add_token(&list, new_token(WORD, ft_strndup(&input[start], *i - start)));
-	return (0);
+	key = ft_strndup(input[*i], *i - start);
+	tmp = env;
+	while (tmp && ft_strcmp(tmp->key, key))
+		tmp = tmp->next;
+	free(key);
+	if (tmp)
+		return (ft_strdup(tmp->value));
+	return (ft_strdup(""));
 }
 
-void	tokenize_input(char *input, t_env *env)
+void	parse_word3(char *input, int *i, t_env *env, t_parse_word *pw)
+{
+	pw->start = *i;
+	while (input[*i] && !is_special(&input[*i]) && input[*i] != ' '	
+			&& input[*i] != '\t' && input[*i] != '\'' && input[*i] != '"' && input[*i] != '$')
+		(*i)++;
+	pw->segment = ft_strndup(&input[pw->start], *i - pw->start);
+	pw->tmp = pw->buf;
+	pw->buf = malloc(ft_strlen(pw->tmp) + ft_strlen(pw->segment) + 1);
+	ft_strcpy(pw.buf, pw.tmp);
+	ft_strcat(pw.buf, pw.segment);
+	free(pw.tmp);
+	free(pw.segment);
+}
+
+void	parse_word2(char *input, int *i, t_env *env, t_parse_word *pw)
+{
+	pw->exp = expand_variable(input, i, env);
+	pw->tmp = pw->buf;
+	pw->buf = malloc(ft_strlen(pw->tmp) + ft_strlen(pw->exp) + 1);
+	ft_strcpy(pw->buf, pw->tmp);
+	ft_strcat(pw->buf, pw->exp);
+	free(pw->tmp);
+	free(pw->exp);
+}
+
+void	parse_word1(char *input, int *i, t_env *env, t_parse_word *pw)
+{
+	pw->quote = input[(*i)++];
+	pw->start = *i;
+	while (input[*i] && input[*i] != pw->quote)
+		(*i)++;
+	pw->segment = ft_strndup(&input[pw->start], *i - pw->start);
+	pw->tmp = pw->buf;
+	pw->buf = malloc(ft_strlen(pw->tmp) + ft_strlen(pw->segment) + 1);
+	ft_strcpy(pw->buf, pw->tmp);
+	ft_strcat(pw->buf, pw->segment);
+	free(pw->tmp);
+	free(pw->segment);
+	if (input[*i] == pw->quote)
+		(*i)++;
+}
+
+char	parse_word(char *input, int *i, t_env *env)
+{
+	t_parse_word	pw;
+
+	pw.buf = ft_calloc(1, 1);
+	pw.start = *i;
+	while (input[*i] && input[*i] != ' ' && input[*i] != '\t' && !ispecial(&input[*i]))
+	{
+		if (input[*i] == '\'' || input[*i] == '"')
+			parse_word1(input, i, env, &pw);
+		else if (input[*i] == '$')
+			parse_word2(input, i, env, &pw);
+		else
+			parse_word3(input, i, env, &pw);
+	}
+	return (pw.buf);
+}
+
+t_token	*tokenize_input(char *input, t_env *env)
 {
 	t_token	*list;
 	int		i;
+	char	*word;
 
 	list = NULL;
 	i = 0;
@@ -31,8 +101,13 @@ void	tokenize_input(char *input, t_env *env)
 		}
 		else
 		{
-			add_token(&list, new_token(WORD, ft_strndup(&input[i], 1)));
+			word = parse_word(input, &i, env);
+			if (word && *word)
+				add_token(&list, new_token(WORD, word));
+			else
+				free(word);
 		}
 	}
-	
+	add_token(&list, new_token(END_OF_INPUT, NULL));
+	return (list);
 }
